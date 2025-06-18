100de097357ef33a3c1cc8d0a7c9c8e732faa6aa
Tanish Mittal <83713094+tansihmittal@users.noreply.github.com>
2025-06-18 13:23:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -976,7 +976,7 @@ class SBIEmailExtractor:
                             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
                             "token_uri": "https://oauth2.googleapis.com/token",
                             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                            "redirect_uris": ["http://localhost"]
+                            "redirect_uris": ["https://sbi-track.streamlit.app/"]
                         }
                     }
                     
